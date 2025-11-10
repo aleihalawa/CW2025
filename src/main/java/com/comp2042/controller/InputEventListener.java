6eb54f47f407d11ec6bc1d34cff1ce4dac92d5c2
Alei Halawa <aleihalawa@gmail.com>
2025-11-10 11:44:34 +0800

    refactor(structure): reorganize classes into MVC packages (no logic changes)

---
+++ src/main/java/com/comp2042/controller/InputEventListener.java
@@ -0,0 +1,18 @@
+package com.comp2042.controller;
+
+import com.comp2042.events.MoveEvent;
+import com.comp2042.model.DownData;
+import com.comp2042.model.ViewData;
+
+public interface InputEventListener {
+
+    DownData onDownEvent(MoveEvent event);
+
+    ViewData onLeftEvent(MoveEvent event);
+
+    ViewData onRightEvent(MoveEvent event);
+
+    ViewData onRotateEvent(MoveEvent event);
+
+    void createNewGame();
+}

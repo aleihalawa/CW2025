542d02d17918dcbf1d85df6900d1336b6d423e6c
Alei Halawa <aleihalawa@gmail.com>
2025-11-25 21:29:54 +0800

    feat(ui): initial main menu implementation with start and quit functionality

--- src/main/java/com/comp2042/app/Main.java
+++ src/main/java/com/comp2042/app/Main.java
@@ -1,7 +1,5 @@
 package com.comp2042.app;
 
-import com.comp2042.controller.GameController;
-import com.comp2042.view.GuiController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,17 +14,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        URL location = getClass().getClassLoader().getResource("gameLayout.fxml");
+        // Load MainMenu.fxml first
+        URL location = getClass().getClassLoader().getResource("com/comp2042/view/MainMenu.fxml");
         ResourceBundle resources = null;
         FXMLLoader fxmlLoader = new FXMLLoader(location, resources);
         Parent root = fxmlLoader.load();
-        GuiController c = fxmlLoader.getController();
 
         primaryStage.setTitle("TetrisJFX");
-        Scene scene = new Scene(root, 650, 600);
+        Scene scene = new Scene(root, 500, 700);
         primaryStage.setScene(scene);
         primaryStage.show();
-        new GameController(c);
     }
 
 
